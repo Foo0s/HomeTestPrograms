a970280221a8efaebc85af2f1699e498e460f244
Foo0s <oraproavp@list.ru>
2022-10-24 22:48:49 +0300

    PZ_5_1
    PZ_5_1

---
+++ Manakova_PZ_5/PZ_5_2_version_2.py
@@ -0,0 +1,19 @@
+def Powerl(A, B):
+    if A < 1:
+        return 0
+    AB
+
+
+    return A
+
+
+try:
+    number_A = float(input('Введите вещественное число: '))
+    number_B = float(input('Введите вещественное число: '))
+    number_C = float(input('Введите вещественное число: '))
+    number_P = float(input('Введите второе вещественное число: '))
+    print(Powerl(number_A, number_P))
+    print(Powerl(number_B, number_P))
+    print(Powerl(number_C, number_P))
+except Exception:
+    print('Ошибка вы ввели не корректное число!')
