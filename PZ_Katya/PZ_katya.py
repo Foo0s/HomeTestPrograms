de2c3e81bcfb7a22483248f88dc008f68ee9eda0
Foo0s <oraproavp@list.ru>
2022-11-07 00:15:54 +0300

    PZ_Katya
    PZ_Katya

---
+++ PZ_Katya/PZ_katya.py
@@ -0,0 +1,19 @@
+def RectPS(x1, y1, x2, y2, P=None, S=None):
+    x = abs(x1- y1)
+    y = abs(x2 - y2)
+    print(f'x -> {x}')
+    print(f'y -> {y}')
+    P = 2*(x-y)
+    S = x*y
+    print(f"Периметр равен: {P}")
+    print(f"Площадь равна: {S}")
+
+try:
+    x_1 = int(input('Введите число для x1 координаты: '))
+    x_2 = int(input('Введите число для x2 координаты: '))
+    y_1 = int(input('Введите число для y1 координаты: '))
+    y_2 = int(input('Введите число для y2 координаты: '))
+except ValueError:
+    print('Ошибка')
+else:
+    RectPS(x_1, y_1, x_2, y_2)
